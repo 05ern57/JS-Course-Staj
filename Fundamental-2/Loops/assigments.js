1b56ffcbf8eec8f37339f906a966caa3b9892706
05ern57 <06ern57@gmail.com>
2023-07-14 18:07:31 +0300

    14.07.23 Cuma-2

--- Fundamental-2/Loops/assigments.js
+++ Fundamental-2/Loops/assigments.js
@@ -8,49 +8,49 @@
 // const tips = [];
 // const totals = [];
 
-// for (let i = 0; i < bills.length; i++) {
-//   // this is the way i do
-//   // tips[i] = calcTip(bills[i]);
-//   // totals[i] = tips[i] + bills[i];
-//   // console.log(tips[i], totals[i]);
+// // for (let i = 0; i < bills.length; i++) {
+// //   // this is the way i do
+// //   // tips[i] = calcTip(bills[i]);
+// //   // totals[i] = tips[i] + bills[i];
+// //   // console.log(tips[i], totals[i]);
 
-//   // and the way Jonas do
-//   tips.push(calcTip(bills[i]));
-//   totals.push(bills[i] + tips[i]);
-//   // he chosed do use push methods insted the way i do
-//   console.log(tips[i], totals[i]);
+// //   // and the way Jonas do
+// //   tips.push(calcTip(bills[i]));
+// //   totals.push(bills[i] + tips[i]);
+// //   // he chosed do use push methods insted the way i do
+// //   console.log(tips[i], totals[i]);
+// // }
+// // console.log(tips, totals);
+
+// // function calcAvarage(arr) {
+// //   let sum = 0;
+// //   for (let i = 0; i < arr.length; i++) {
+// //     sum += arr[i];
+// //   }
+// //   return sum / arr.length;
+// // }
+
+// // console.log(calcAvarage(totals));
+
+// // for (let i = 1; i < 51; i++) {
+// //   console.log(`voter ${i} is currently voting`);
+// // }
+
+// const pop = [3215613213, 5463215454, 32321361321, 654654];
+// const percenatges = [];
+
+// const percentageOfWorld2 = function (population) {
+//   const w = 7900000000;
+//   return (population / w) * 100;
+// };
+
+// for (let i = 0; i < pop.length; i++) {
+//   percenatges.push(percentageOfWorld2(pop[i]));
+//   console.log(percenatges);
 // }
-// console.log(tips, totals);
 
-// function calcAvarage(arr) {
-//   let sum = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     sum += arr[i];
-//   }
-//   return sum / arr.length;
-// }
-
-// console.log(calcAvarage(totals));
-
-// for (let i = 1; i < 51; i++) {
-//   console.log(`voter ${i} is currently voting`);
-// }
-
-const pop = [3215613213, 5463215454, 32321361321, 654654];
-const percenatges = [];
-
-const percentageOfWorld2 = function (population) {
-  const w = 7900000000;
-  return (population / w) * 100;
-};
-
-for (let i = 0; i < pop.length; i++) {
-  percenatges.push(percentageOfWorld2(pop[i]));
-  console.log(percenatges);
-}
-
-const listOfNeighbours = [
-  ["Canada", "Mexico"],
-  ["Spain"],
-  ["Norway", "Sweden", "Russia"],
-];
+// const listOfNeighbours = [
+//   ["Canada", "Mexico"],
+//   ["Spain"],
+//   ["Norway", "Sweden", "Russia"],
+// ];
--- Fundamental-2/Loops/new.js
+++ Fundamental-2/Loops/new.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function findHigher(arr) {
+  let max = arr[0];
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  console.log(max);
+  console.log(min);
+}
+
+findHigher(a);
+findHigher([3, 7, 5]);
